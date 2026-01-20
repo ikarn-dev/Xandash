8baabfb02016703fb1bdcd412fa46878950bd6b0
Karan <ikarn.dev@gmail.com>
2026-01-20 20:24:58 +0530

    Remove names

--- src/libs/utils/node-names.ts
+++ src/libs/utils/node-names.ts
@@ -2,9 +2,9 @@
 const NODE_NAMES: Record<string, string> = {
   '3GF69LzCEfPK8RDc4knSSvhZx8eug2m7TDdyVaCip9Wa': 'Karan',
   '9QAGbt1kLfQDLzfgokbbpP4cYT15pcG9bqf7odQZxC9z': 'Ymetro',
-  'HHptBfyfRPofH97v71np8pXJnYap1U4QGYer1DWkBhCN': 'SubstanceM2 (Peter)',
-  '12z6qfHXTW39rBU3HAL8Q2MEb7Q4ZebwiU9qPqm3ePw5': 'SubstanceM1 (Peter)',
-  '2xSVHT4JAyvpmVRVR9fgNnJKRqKGLaieZwxc2sNa4hW8': 'SubstanceM3 (Peter)',
+  'HHptBfyfRPofH97v71np8pXJnYap1U4QGYer1DWkBhCN': 'SubstanceM2',
+  '12z6qfHXTW39rBU3HAL8Q2MEb7Q4ZebwiU9qPqm3ePw5': 'SubstanceM1',
+  '2xSVHT4JAyvpmVRVR9fgNnJKRqKGLaieZwxc2sNa4hW8': 'SubstanceM3',
 };
 
 export function getNodeName(pubkey: string | null | undefined): string {
